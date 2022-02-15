525428909a08b6d79c28af7e70a6b966d6bf737b
haily <lycth@rabiloo.com>
2022-02-15 10:41:46 +0700

    middleware layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,11 @@
     <nuxt/>
   </div>
 </template>
+<script>
+  export default{
+    middleware: 'always'
+  }
+</script>
 
 <style>
 html {
--- middleware/always.js
+++ middleware/always.js
@@ -1,3 +1,3 @@
 export default function(context){
-    console.log("middleware always")
+    console.log("middleware layouts")
 }
