5f2dddebbb74cb81d96ee4ebb99785adfbf946c8
haily <lycth@rabiloo.com>
2022-02-15 09:46:13 +0700

    nuxt lifecycle

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,69 @@
 <template>
-  <Tutorial/>
+  <section class="container">
+    <div>
+      <nuxt-logo/>
+      <h1 class="title">
+        nuxt-lifecycle-example
+      </h1>
+      <h2 class="subtitle">
+        <nuxt-link to="/page2">page2</nuxt-link>
+      </h2>
+    </div>
+  </section>
 </template>
 
 <script>
+import NuxtLogo from '~/components/NuxtLogo.vue'
 export default {
-  name: 'IndexPage'
+  components: { NuxtLogo },
+  name: 'IndexPage',
+  middleware :['page2', 'page1'],
+   created() {
+    console.log("page created")
+  },
+  mounted() {
+    console.log("page mounted")
+  },
+  fetch (context) {
+    console.log("page fetch")
+  },
+  validate (context) {
+    console.log("page validate")
+    return true
+  },
+  asyncData (context) {
+    console.log("page asyncData")
+  }
+
 }
 </script>
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
